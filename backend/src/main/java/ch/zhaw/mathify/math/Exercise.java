fee81798881da9dbf2a721f8b75ebdb3c8bfec16
zehndjon <zehndjon@students.zhaw.ch>
2024-03-15 21:38:48 +0100

    Revert "Structure of the factory"
    This reverts commit cb930b32b4d43c14b7f60b5b50c4b071689f440c.

--- backend/src/main/java/ch/zhaw/mathify/math/Exercise.java
+++
@@ -1,6 +0,0 @@
-package ch.zhaw.mathify.math;
-
-public interface Exercise {
-    public Grade getGrade();
-    public double getResult();
-}
--- backend/src/main/java/ch/zhaw/mathify/math/ExerciseType.java
+++
@@ -1,5 +0,0 @@
-package ch.zhaw.mathify.math;
-
-public enum ExerciseType {
-    ARITHMETIC
-}
