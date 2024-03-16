1f428f251f85c934d33060711abd6f2080637449
zehndjon <zehndjon@students.zhaw.ch>
2024-03-16 18:05:18 +0100

    restructured the expression part

--- backend/src/main/java/ch/zhaw/mathify/math/exercise/ArithmeticExercise.java
+++ backend/src/main/java/ch/zhaw/mathify/math/exercise/ArithmeticExercise.java
@@ -1,7 +1,6 @@
 package ch.zhaw.mathify.math.exercise;
 
 public class ArithmeticExercise extends Exercise {
-    @Override
     public double getResult() {
         throw new UnsupportedOperationException("Not implemented yet");
     }
