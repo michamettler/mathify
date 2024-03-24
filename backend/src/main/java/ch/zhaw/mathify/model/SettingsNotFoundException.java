f0f6bdf03cd50563b808df3a040079cb41a5ccaf
zehndjon <zehndjon@students.zhaw.ch>
2024-03-24 15:00:02 +0100

    Javadoc

--- backend/src/main/java/ch/zhaw/mathify/model/SettingsNotFoundException.java
+++ backend/src/main/java/ch/zhaw/mathify/model/SettingsNotFoundException.java
@@ -1,5 +1,8 @@
 package ch.zhaw.mathify.model;
 
+/**
+ * Exception that is thrown when the settings.json file is not found
+ */
 public class SettingsNotFoundException extends Exception{
     public SettingsNotFoundException(String message) {
         super(message);
