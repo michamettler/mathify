79faddf116947106f8777660ab5f06839afcb6b0
mettlmi1 <mettlermicha@gmail.com>
2024-03-29 19:57:03 +0100

    added base user registration page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {
+  UserRegistrationComponent
+} from "./features/registration/components/user-registration/user-registration.component";
+import {GradeSelectionComponent} from "./features/registration/components/grade-selection/grade-selection.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: UserRegistrationComponent},
+  { path: 'login', component: UserRegistrationComponent},
+  { path: 'grade-selection', component: GradeSelectionComponent}
+]
--- frontend/src/app/features/registration/components/user-registration/user-registration.component.ts
+++ frontend/src/app/features/registration/components/user-registration/user-registration.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-user-registration',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './user-registration.component.html',
   styleUrl: './user-registration.component.scss'
 })
 export class UserRegistrationComponent {
+  userName: string = '';
+  password: string = '';
+
+  constructor(private router: Router) {
+  }
+
+  login(): void {
+    console.log('Login', this.userName, this.password);
+    //TODO implement login logic here
+    this.router.navigate(['/grade-selection']);
+  }
+
+  register(): void {
+    //TODO implement register logic here
+    console.log('Register', this.userName, this.password);
+  }
+
 
 }
