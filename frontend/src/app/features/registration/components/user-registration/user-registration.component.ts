36a647d00030a6e975cf532ed41b99cb9cc1d9f5
mettlmi1 <mettlermicha@gmail.com>
2024-04-04 20:50:24 +0200

    added registration

--- frontend/src/app/features/registration/components/user-registration/user-registration.component.ts
+++ frontend/src/app/features/registration/components/user-registration/user-registration.component.ts
@@ -3,6 +3,7 @@ import {FormsModule} from "@angular/forms";
 import {Router} from "@angular/router";
 import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
 import {MatInput, MatInputModule} from "@angular/material/input";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-user-registration',
@@ -11,7 +12,8 @@ import {MatInput, MatInputModule} from "@angular/material/input";
     FormsModule,
     MatFormField,
     FormsModule,
-    MatFormFieldModule, MatInputModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatInput
   ],
   templateUrl: './user-registration.component.html',
@@ -21,19 +23,21 @@ export class UserRegistrationComponent {
   userName: string = '';
   password: string = '';
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private _snackBar: MatSnackBar) {
   }
 
   login(): void {
     console.log('Login', this.userName, this.password);
-    //TODO implement login logic here
-    this.router.navigate(['/grade-selection']);
+    //TODO send data to backend
+    if (this.userName && this.password) {
+      this.router.navigate(['/grade-selection']);
+    }
   }
 
   register(): void {
-    //TODO implement register logic here
     console.log('Register', this.userName, this.password);
+    //TODO send data to backend
+    this._snackBar.open("User has been created! You can now log in.", "dismiss", {verticalPosition: 'top', horizontalPosition: 'end'});
   }
-
 
 }
