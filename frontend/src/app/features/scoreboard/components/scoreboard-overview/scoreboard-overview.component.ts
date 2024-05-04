57be95d3519e02147a1b625ddedd2fdc5077c002
michamettler <mettlermicha@gmail.com>
2024-05-04 17:01:40 +0200

    base implementation of calling the scoreboard endpoint from ui

--- frontend/src/app/features/scoreboard/components/scoreboard-overview/scoreboard-overview.component.ts
+++ frontend/src/app/features/scoreboard/components/scoreboard-overview/scoreboard-overview.component.ts
@@ -1,5 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HeaderComponent} from "../../../../core/components/header/header.component";
+import {Router} from "@angular/router";
+import {Title} from "@angular/platform-browser";
+import {ScoreboardOverviewService} from "../../services/scoreboard-overview.service";
 
 @Component({
   selector: 'app-scoreboard-overview',
@@ -10,6 +13,18 @@ import {HeaderComponent} from "../../../../core/components/header/header.compone
   templateUrl: './scoreboard-overview.component.html',
   styleUrl: './scoreboard-overview.component.scss'
 })
-export class ScoreboardOverviewComponent {
+export class ScoreboardOverviewComponent implements OnInit {
+
+  constructor(private router: Router, private titleService: Title, private scoreboardOverviewService: ScoreboardOverviewService) {
+    this.titleService.setTitle('Mathify!');
+  }
+
+  ngOnInit(): void {
+    this.scoreboardOverviewService.getScoreboard().subscribe({
+      next: (response) => {
+       console.log(response);
+      }
+    });
+  }
 
 }
--- frontend/src/app/features/scoreboard/services/scoreboard-overview.service.ts
+++ frontend/src/app/features/scoreboard/services/scoreboard-overview.service.ts
@@ -1,10 +1,33 @@
 import {Injectable} from '@angular/core';
+import {catchError, Observable, throwError} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScoreboardOverviewService {
 
-  constructor() {
+  private apiUrl = 'https://localhost/api';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getScoreboard(): Observable<any> {
+    let url = `${this.apiUrl}scoreboard`;
+
+    const headers = new HttpHeaders({
+      'Authorization': localStorage.getItem('token')?.toString() + '',
+    });
+    return this.http.get(url, {headers: headers})
+      .pipe(
+        catchError(this.handleError('scoreboard'))
+      );
+  }
+
+  private handleError(operation = 'operation') {
+    return (error: any): Observable<any> => {
+      console.error(`${operation} failed: ${error.message}`);
+      return throwError(() => new Error(`${operation} failed: ${error.message}`));
+    };
   }
 }
