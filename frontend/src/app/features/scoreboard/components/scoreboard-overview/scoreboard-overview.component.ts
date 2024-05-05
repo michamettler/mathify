dd953f712a1124fd95af97c544f0ce4ea44f82cb
michamettler <mettlermicha@gmail.com>
2024-05-05 16:18:25 +0200

    implemented scoreboard

--- frontend/src/app/features/scoreboard/components/scoreboard-overview/scoreboard-overview.component.ts
+++ frontend/src/app/features/scoreboard/components/scoreboard-overview/scoreboard-overview.component.ts
@@ -3,17 +3,51 @@ import {HeaderComponent} from "../../../../core/components/header/header.compone
 import {Router} from "@angular/router";
 import {Title} from "@angular/platform-browser";
 import {ScoreboardOverviewService} from "../../services/scoreboard-overview.service";
+import {User} from "../../../../../model/user";
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatRow,
+  MatRowDef,
+  MatTable
+} from "@angular/material/table";
+import {MatButton} from "@angular/material/button";
+
+export interface DisplayUser extends User {
+  position: number;  // Add position for display purposes
+}
 
 @Component({
   selector: 'app-scoreboard-overview',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatHeaderRow,
+    MatRow,
+    MatCellDef,
+    MatHeaderCellDef,
+    MatHeaderRowDef,
+    MatRowDef,
+    MatButton
   ],
   templateUrl: './scoreboard-overview.component.html',
   styleUrl: './scoreboard-overview.component.scss'
 })
 export class ScoreboardOverviewComponent implements OnInit {
+
+  users: DisplayUser[] = [];
+
+  displayedColumns: string[] = ['position', 'username', 'level', 'experience', 'grade'];
+  dataSource: DisplayUser[] = [];
 
   constructor(private router: Router, private titleService: Title, private scoreboardOverviewService: ScoreboardOverviewService) {
     this.titleService.setTitle('Mathify!');
@@ -22,9 +56,18 @@ export class ScoreboardOverviewComponent implements OnInit {
   ngOnInit(): void {
     this.scoreboardOverviewService.getScoreboard().subscribe({
       next: (response) => {
-       console.log(response);
-      }
+        const reversedData = response.slice().reverse();
+        this.dataSource = reversedData.map((user: any, index: number) => ({
+          ...user,
+          position: index + 1
+        }));
+      },
+      error: (err) => console.error('Failed to load scoreboard:', err)
     });
+  }
+
+  startGame() {
+    this.router.navigate(['/exercise']);
   }
 
 }
